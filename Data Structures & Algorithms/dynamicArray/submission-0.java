e5660820a12829a78f72cb8dbe9fcaa4669cfb35
Aryan Mishra <139005437+AryanM1223@users.noreply.github.com>
2026-04-15 02:28:30 +0530

    Add: dynamicArray - submission-0

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.java
@@ -0,0 +1,51 @@
+class DynamicArray{
+    int capacity;
+    int[] arr;
+    int size;
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.arr = new int[capacity];
+    }
+
+    public int get(int i) {
+        return arr[i];
+    }
+
+    public void set(int i, int n) {
+       arr[i] = n;
+    }
+
+    public void pushback(int n) {
+        if(size == capacity){
+            resize();
+        }
+        arr[size] = n;
+        size++;
+    }
+
+    public int popback() {
+        if(size > 0){
+            size--;
+        }
+        return arr[size];
+    }
+
+    public void resize() {
+        capacity = capacity * 2;
+        int[] arr2 = new int[capacity];
+        for(int i = 0; i < size; i++){
+            arr2[i] = arr[i];
+        }
+        arr = arr2;
+        
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
